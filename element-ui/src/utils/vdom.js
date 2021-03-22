6f52008ef16b2707a7d547bf404b02072b7208d7
QianZi996 <1464451589@qq.com>
2021-03-22 17:49:36 +0800

    Site updated: 2021-03-22 17:49:35

--- element-ui/src/utils/vdom.js
+++
@@ -1,5 +0,0 @@
-import { hasOwn } from 'element-ui/src/com.com.itheima.utils/util';
-
-export function isVNode(node) {
-  return node !== null && typeof node === 'object' && hasOwn(node, 'componentOptions');
-};
